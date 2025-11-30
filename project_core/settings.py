010754d6a4277d713a3d21f3f59290b4758a9591
URIAN VIERA <urian1213viera@gmail.com>
2025-11-29 20:47:33 -0500

    corrijiendo error

--- project_core/settings.py
+++ project_core/settings.py
@@ -11,17 +11,16 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import environ
-
 import os
-
-# Inicializa entorno
-env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Inicializa entorno
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
